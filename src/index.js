6764aa7f9c9668625bda2eb883a5d4c619ecae23
Ruan Pablo <ruan_goio10@hotmail.com>
2022-05-13 21:27:54 -0300

    Get user statement

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.use(express.json());
 
 const custumers = [];
 
+app.listen(3333, () => {
+    console.log('Server open on port 3333 🔥');
+});
+
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
 
@@ -25,6 +29,14 @@ app.post("/account", (req, res) => {
     return res.status(201).send();
 });
 
-app.listen(3333, () => {
-    console.log('Server open on port 3333 🔥');
+app.get("/statement", (req, res) => {
+    const { cpf } = req.headers;
+
+    const custumer = custumers.find(custumer => custumer.cpf === cpf);
+
+    if (!custumer) {
+        return res.status(400).json({ error: "Custumer not found" });
+    }
+
+    return res.json(custumer.statement);
 });
